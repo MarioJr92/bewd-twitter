77d69a8d0b1c73dcf5f047425d041471f2c69581
MarioJr92 <mariorodjr92@gmail.com>
2022-09-08 06:50:45 -0400

    created Tweets controller with create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,35 @@
 class TweetsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    token = cookies.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      user = session.user
+      @tweet = user.tweets.new(tweet_params)
+
+      if @tweet.save
+        render json: {
+          tweet: {
+            username: user.username,
+            message: @tweet.message,
+          }
+        }
+      else
+        render json: {
+          success: false,
+        }
+      end
+    else
+      render json: {
+        success: false,
+      }
+    end
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:message)
+    end
 end
