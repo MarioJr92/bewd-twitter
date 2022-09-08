9094ad78ba03609b3eca7f15f272d42becc6aafa
MarioJr92 <mariorodjr92@gmail.com>
2022-09-08 06:25:50 -0400

    created Sessions controller with create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && (@user.password == params[:user][:password])
+      session = @user.sessions.create
+
+      cookies.permanent.signed[:twitter_session_token] = {
+        value: session.token,
+        httponly: true,
+      }
+
+      render json: {
+        success: true,
+      }
+    else
+      render json: {
+        success: false,
+      }
+    end
+  end
 end
