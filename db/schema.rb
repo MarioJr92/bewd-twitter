aa04e1704165623b2abe95967a9e8ba2a81cb328
MarioJr92 <mariorodjr92@gmail.com>
2022-09-08 05:45:39 -0400

    created Sessions table/model and added user id to sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_08_091855) do
+ActiveRecord::Schema.define(version: 2022_09_08_093725) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2022_09_08_091855) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "sessions", "users"
 end
